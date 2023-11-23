9a1db78463b3f2524a820dab066238b62ca9e734
Claudio Ponczek <duckartz13240@gmail.com>
2023-11-23 13:42:50 -0300

    Adc-btn-camp

--- apiCampeonato/FRONT/src/app/app-routing.module.ts
+++ apiCampeonato/FRONT/src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from "@angular/router";
 import { TimeListarComponent } from "./pages/time/time-listar/time-listar.component";
 import { TimeCadastrarComponent } from "./pages/time/time-cadastrar/time-cadastrar.component";
 import { TimeAlterarComponent } from "./pages/time/time-alterar/time-alterar.component";
+import { CampeonatoCadastrarComponent } from "./pages/campeonato/campeonato-cadastrar/campeonato-cadastrar.component";
+import { CampeonatoListarComponent } from "./pages/campeonato/campeonato-listar/campeonato-listar.component";
 
 const routes: Routes = [
   {
@@ -20,6 +22,14 @@ const routes: Routes = [
   {
     path: "pages/time/alterar/:id",
     component: TimeAlterarComponent,
+  },
+  {
+    path: "pages/campeonato/cadastrar",
+    component: CampeonatoCadastrarComponent,
+  },
+  {
+    path: "pages/campeonato/listar",
+    component: CampeonatoListarComponent,
   },
 ];
 
