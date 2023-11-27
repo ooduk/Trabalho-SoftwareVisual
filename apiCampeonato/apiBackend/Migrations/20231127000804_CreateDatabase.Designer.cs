7e2c6d200ac3c8ea46757f711f78c83768dd9590
IansenUP <IansenUP@gmail.com>
2023-11-26 21:08:27 -0300

    FixErrors

--- apiCampeonato/apiBackend/Migrations/20231127000804_CreateDatabase.Designer.cs
+++ apiCampeonato/apiBackend/Migrations/20231127000804_CreateDatabase.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace apiBackend.Migrations
 {
     [DbContext(typeof(AppDataContext))]
-    [Migration("20231127000413_CreateDatabase")]
+    [Migration("20231127000804_CreateDatabase")]
     partial class CreateDatabase
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
